ab896901ff4960934ccb856b2f26dd79242d6ab0
KuritsuM <dyma9111@gmail.com>
2022-07-05 20:34:45 +0300

    Initial commit, first task completed

---
+++ src/test/java/com/kaydash_dmitriy/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaydash_dmitriy.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
